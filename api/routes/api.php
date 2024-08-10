2713857b52c46a78ee0ad0f950c44a83f7e07254
adeelch9 <adeel.chaudhary@tron.network>
2024-08-10 05:37:49 -0400

    Revert "Merge pull request #4 from adeelch9/tron-integration"
    This reverts commit f2f5556516bcc260e9620be761260b1ab2cc8479, reversing
    changes made to 44455494ae2d8b53e64885f1a6af3843f1d74869.

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,11 +9,4 @@ Route::any('/transactions',     [ API::class, 'Transactions' ]); // complete
 Route::post('/transfer',        [ API::class, 'Transfer'     ]); // complete
 Route::post('/verify',          [ API::class, 'Verify'       ]); // complete
 Route::post('/generate',        [ API::class, 'Generate'     ]); // complete
-Route::get('/run-migrations', function() {
-    Artisan::call('optimize:clear');
-    Artisan::call('migrate');
-
-    return 'done';
-});
 Route::get('/{any}',            [ API::class, 'Invalid'      ])->where([ 'any' => '.*' ]); // complete
-
